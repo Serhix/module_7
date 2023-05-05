280c88b982487358e56d464c29f5eaa036287b0f
Strategs <127782103+Strategs@users.noreply.github.com>
2023-05-05 11:18:37 +0300

    Add

---
+++ clean_folder/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_namespace_packages
+
+setup(name='clean_folder',
+      version='0.0.1',
+      description='Script for sorting files in a folder',
+      author='Serhii Hrabar',
+      author_email='strategs89@gmail.com',
+      license='MIT',
+      packages=find_namespace_packages(),
+      entry_points={'console_scripts': ['clean-folder=clean_folder.clean:clean_folder']},
+      # clean-folder - команда, яка повинна виконатись в терміналі
+      # після '=' пишемо шлях до файлу, де знаходиться функція -> hello_world_hsv.main
+      # після ':' пишемо назву функції яку запускати треба
+      classifiers=[
+          'Operating System :: Microsoft :: Windows',
+          'Programming Language :: Python',
+          ]       
+      )
